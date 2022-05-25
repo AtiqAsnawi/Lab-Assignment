b25f4bf329e85e169f47fa3d96011f91a9b624c2
Atiq Asnawi <atiqjumali123@gmail.com>
2022-05-25 13:24:09 -0400

    user input

---
+++ userinput.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+/*this is my second program in C */
+int age;
+printf("HI, how old are you? > ");
+scanf("%d", &age);
+printf("you are %d years old", age);
+return 0;
+}
